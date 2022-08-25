bbde2ad778c9939934ba041a9c133f63cd3e9302
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2022-08-25 15:01:02 +0200

    feat: Add dye function.
    To change the color of text for command line.

---
+++ dye/config.go
@@ -0,0 +1,8 @@
+package dye
+
+// Config is to save the essential data like the color
+// And the build value to return the data
+type Config struct {
+	Color string
+	Build []byte
+}
---
+++ dye/execute.go
@@ -0,0 +1,33 @@
+package dye
+
+import "fmt"
+
+// start, reset are to generate the color code
+const (
+	start = "\033["
+	reset = "\033[0m"
+)
+
+// color is to map the color name to the code
+var (
+	color = map[string]string{
+		"red":    "31m",
+		"green":  "32m",
+		"yellow": "33m",
+		"blue":   "34m",
+		"purple": "35m",
+		"cyan":   "36m",
+	}
+)
+
+// Execute is to dye the text and return it as byte slice
+func (c Config) Execute(text string) []byte {
+
+	c.Build = fmt.Append(c.Build, start)
+	c.Build = fmt.Append(c.Build, color[c.Color])
+	c.Build = fmt.Append(c.Build, text)
+	c.Build = fmt.Append(c.Build, reset)
+
+	return c.Build
+
+}
