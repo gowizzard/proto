5bf4e0a9b4fd80d34be72a42ba52a43884d709b3
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2022-08-26 09:11:45 +0200

    test: Add testing function for log.

---
+++ log_test.go
@@ -0,0 +1,47 @@
+package proto_test
+
+import (
+	"github.com/gowizzard/proto"
+	"testing"
+)
+
+func TestLog(t *testing.T) {
+
+	tests := []struct {
+		Config     proto.Config
+		Kind       string
+		Message    string
+		Attributes map[string]any
+	}{
+		{
+			Config: proto.Config{
+				CommandLine: true,
+			},
+			Kind:       "info",
+			Message:    "This is an informational message.",
+			Attributes: map[string]any{},
+		},
+		{
+			Config: proto.Config{
+				CommandLine:     true,
+				FileInformation: true,
+			},
+			Kind:    "error",
+			Message: "This is an error message.",
+			Attributes: map[string]any{
+				"error": "wrong type",
+			},
+		},
+	}
+
+	for _, value := range tests {
+
+		c := value.Config
+		err := c.Log(value.Kind, value.Message, value.Attributes)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+	}
+
+}
